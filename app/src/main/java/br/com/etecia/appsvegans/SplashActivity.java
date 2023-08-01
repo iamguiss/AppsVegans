231499eb7dc526893e5b6ac6a297940f6abb20eb
iamguiss <guizinhosilvasantos10@gmail.com>
2023-08-01 20:10:48 -0300

    App tela splash feita

--- app/src/main/java/br/com/etecia/appsvegans/SplashActivity.java
+++ app/src/main/java/br/com/etecia/appsvegans/SplashActivity.java
@@ -2,7 +2,12 @@ package br.com.etecia.appsvegans;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import br.com.etecia.appsvegans.LoginActivity;
+import br.com.etecia.appsvegans.R;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash__layout);
+
+        //criando o carregamento da janela de login
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //abrindo a janela de login
+                startActivity(new Intent(getApplicationContext(),
+                        LoginActivity.class));
+                finish();//fechando a janela atual
+            }
+        }, 3000);
+
+
     }
 }
